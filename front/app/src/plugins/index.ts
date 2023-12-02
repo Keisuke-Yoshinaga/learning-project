78554c0ed002c79270705ddfc00039bdffbae90c
Yoshinaga Keisuke <k.yoshinaga369@outlook.com>
2023-12-03 05:05:05 +0900

    toast追加

--- front/app/src/plugins/index.ts
+++ front/app/src/plugins/index.ts
@@ -7,10 +7,11 @@
 // Plugins
 import vuetify from "@/plugins/vuetify";
 import router from "@/router";
+import ToastPlugin from "vue-toast-notification";
 
 // Types
 import type { App } from "vue";
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router);
+  app.use(vuetify).use(router).use(ToastPlugin);
 }
